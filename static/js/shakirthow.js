03e7d6c8701009f1ae5c0e19c63f6e468123c197
Shakir Thowseen <shakirthowseen@Vanillas-iMac.local>
2015-02-17 17:48:15 -0500

    Added Work Exp Section with horizontal scroll

--- static/js/shakirthow.js
+++ static/js/shakirthow.js
@@ -1,11 +1,84 @@
 $( document ).ready(function() {
 	console.log( "ready!" );
 	rotateText()
-	plane()
+	// plane()
 
-
+	workExpMagic()
 
 });
+
+
+// set correct size for wipe child
+// var resizeWipeChild = function () {
+//     $("#pin .wipe h3").width($(window).width());
+// };
+// $(window).on("resize", resizeWipeChild);
+
+// 		var controller;
+// 	$(document).ready(function ($) {
+// 	    // init controller
+// 	    controller = new ScrollMagic();
+//         resizeWipeChild(); //trigger once
+// 	});
+
+
+
+
+	// build tween
+
+
+
+
+// 	 $(document).ready(function ($) {
+
+
+
+
+
+
+function workExpMagic(){
+	controller = new ScrollMagic();
+	controller2 = new ScrollMagic();
+
+	var scenePin = new ScrollScene({triggerElement: "#workExpMagicTrigger", duration: 300, pushFollowers: false})
+	.setPin("#workExp")
+	.addTo(controller);
+
+
+	var preview = new TimelineMax()
+	preview.add([TweenMax.to("#beachIntro", 1, {
+		width: "100%"
+	})])
+	preview.add(TweenMax.to("#desertIntro", 1, {
+		width: "100%"
+	}))
+	preview.add([TweenMax.to("#mountainIntro", 1, {
+		width: "100%"
+	})])
+	preview.add(TweenMax.to("#valleyIntro", 1, {
+		width: "100%"
+	}))
+	preview.add([TweenMax.to("#redwoodIntro", 1, {
+		width: "100%"
+	})])
+
+
+	var sceneWipe = new ScrollScene({
+		triggerElement: "#workExpMagicTrigger",
+		duration: 300
+	}).setTween(preview).setPin("#pin").addTo(controller2);
+
+
+	// controller.addScene([ scenePin])
+	scenePin.addIndicators();
+	sceneWipe.addIndicators();
+
+}
+
+
+
+
+
 
 function plane(){
 	var targetPos = $('#target').position()
@@ -22,7 +95,7 @@ function plane(){
 			{x: 300,	y: 10}
 			// {x: namePos.left, y:-offset},
 		 // 	{x: 300,	y: 10}
-			]
+		 ]
 		},
 		looping : {
 			curviness: 1.25,
@@ -72,57 +145,57 @@ function plane(){
 
 
 
-	function rotateText(){
-		var TxtRotate = function(el, toRotate, period) {
-			this.toRotate = toRotate;
-			this.el = el;
-			this.loopNum = 0;
-			this.period = parseInt(period, 10) || 2000;
-			this.txt = '';
-			this.tick();
+function rotateText(){
+	var TxtRotate = function(el, toRotate, period) {
+		this.toRotate = toRotate;
+		this.el = el;
+		this.loopNum = 0;
+		this.period = parseInt(period, 10) || 2000;
+		this.txt = '';
+		this.tick();
+		this.isDeleting = false;
+	};
+
+	TxtRotate.prototype.tick = function() {
+		var i = this.loopNum % this.toRotate.length;
+		var fullTxt = this.toRotate[i];
+
+		if (this.isDeleting) {
+			this.txt = fullTxt.substring(0, this.txt.length - 1);
+		} else {
+			this.txt = fullTxt.substring(0, this.txt.length + 1);
+		}
+
+		this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+
+		var that = this;
+		var delta = 300 - Math.random() * 100;
+
+		if (this.isDeleting) { delta /= 2; }
+
+		if (!this.isDeleting && this.txt === fullTxt) {
+			delta = this.period;
+			this.isDeleting = true;
+		} else if (this.isDeleting && this.txt === '') {
 			this.isDeleting = false;
-		};
+			this.loopNum++;
+			delta = 500;
+		}
 
-		TxtRotate.prototype.tick = function() {
-			var i = this.loopNum % this.toRotate.length;
-			var fullTxt = this.toRotate[i];
+		setTimeout(function() {
+			that.tick();
+		}, delta);
+	};
 
-			if (this.isDeleting) {
-				this.txt = fullTxt.substring(0, this.txt.length - 1);
-			} else {
-				this.txt = fullTxt.substring(0, this.txt.length + 1);
+	window.onload = function() {
+		var elements = document.getElementsByClassName('txt-rotate');
+		for (var i=0; i<elements.length; i++) {
+			var toRotate = elements[i].getAttribute('data-rotate');
+			var period = elements[i].getAttribute('data-period');
+			if (toRotate) {
+				new TxtRotate(elements[i], JSON.parse(toRotate), period);
 			}
-
-			this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
-
-			var that = this;
-			var delta = 300 - Math.random() * 100;
-
-			if (this.isDeleting) { delta /= 2; }
-
-			if (!this.isDeleting && this.txt === fullTxt) {
-				delta = this.period;
-				this.isDeleting = true;
-			} else if (this.isDeleting && this.txt === '') {
-				this.isDeleting = false;
-				this.loopNum++;
-				delta = 500;
-			}
-
-			setTimeout(function() {
-				that.tick();
-			}, delta);
-		};
-
-		window.onload = function() {
-			var elements = document.getElementsByClassName('txt-rotate');
-			for (var i=0; i<elements.length; i++) {
-				var toRotate = elements[i].getAttribute('data-rotate');
-				var period = elements[i].getAttribute('data-period');
-				if (toRotate) {
-					new TxtRotate(elements[i], JSON.parse(toRotate), period);
-				}
-			}
+		}
   // INJECT CSS
   var css = document.createElement("style");
   css.type = "text/css";
